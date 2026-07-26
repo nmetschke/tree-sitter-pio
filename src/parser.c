eea5b9b5a747899971d4902cbff44ea6de47a769
Niclas Metschke <niclas.metschke@protonmail.com>
2026-07-26 17:47:16 +0000

    Regenerate parser

--- src/parser.c
+++ src/parser.c
@@ -8805,7 +8805,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_pio(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
