1013bfa01535711336bf7627711a9baa531f2c43
Niclas Metschke <niclas.metschke@protonmail.com>
2026-07-31 18:24:10 +0200

    Release v0.2.0

--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 { lib, buildGrammar }:
 buildGrammar {
   language = "pio";
-  version = "0.1.1";
+  version = "0.2.0";
   src = lib.sources.cleanSource ./.;
   meta = {
     license = lib.licenses.mit;
