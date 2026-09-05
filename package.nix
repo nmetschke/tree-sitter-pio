1c706c5fab3b5858422158bc4a8d54a0fc8308f1
Niclas Metschke <niclas.metschke@protonmail.com>
2026-09-05 15:03:22 +0200

    update tree-sitter to 0.27

--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 { lib, buildGrammar }:
 buildGrammar {
   language = "pio";
-  version = "0.2.0";
+  version = "0.2.1";
   src = lib.sources.cleanSource ./.;
   meta = {
     license = lib.licenses.mit;
